aa2860d6f239c155d61013b8387cf5730270bed3
priyanshugupta69 <priyanshuraja456@gmail.com>
2023-07-16 07:51:48 +0100

    final files added

--- mulInmulOut.py
+++ mulInmulOut.py
@@ -2,32 +2,37 @@ import threading
 import time
 import RPi.GPIO as GPIO
 import wavGen
-import wavRec1
+import wavRecForMulInOut
+import os
 
-def thread1_function(pin , frequency):
+def thread1_function(pin, frequency):
     # Code for thread 1
-    wavGen.wavGenerator(pin , frequency)
+    wavGen.wavGenerator(pin, frequency)
 
 # Define a function to be run as thread 2
-def thread2_function(channel  ,filename):
+def thread2_function(channel, filename):
     # Code for thread 2
-    wavRec1.save_sensor_data_to_file(channel , filename)
+    wavRecForMulInOut.save_sensor_data_to_file(channel, filename)
 
 pins = [11, 13, 15, 16]
 channels = [0, 1, 2, 3]
-transducers = 4
+transducers = 0
+frequency = 750
 directory = "sensor_data"
 timestamp = time.strftime("%Y%m%d%H%M%S")  # Get the current timestamp
-filename = os.path.join(directory, f"sensor_data_{timestamp}.txt")
+filename = os.path.join(directory, f"sensor_data{timestamp}.csv")
 
-while transducers >= 0:
+for i in range(transducers, len(pins)):
     threads = []
-    frequency = 1500
-    for i in range(transducers, len(pins)):
-        threads.append(threading.Thread(target=thread1_function, args=(pins[i],frequency,)))
-        for j in range(i + 1, len(channels)):
-            threads.append(threading.Thread(target=thread2_function, args=(channels[j], filename,)))
-
+    threads.append(threading.Thread(target=thread1_function, args=(pins[i], frequency,)))
+    for j in range(i + 1, len(channels)):
+        threads.append(threading.Thread(target=thread2_function, args=(channels[j], filename,)))
     for thread in threads:
         thread.start()
-    transducers-= 1
+    for thread in threads:
+        thread.join()
+    threads.clear()
+    transducers += 1
+
+print("Data is stored successfully.")
+
--- test.py
+++ test.py
@@ -1,10 +1,8 @@
 import time
 import os
-directory = "sensor_data"
-timestamp = time.strftime("%Y%m%d%H%M%S")  # Get the current timestamp
-filename = os.path.join(directory, f"sensor_data_20230714163915.csv")
-import wavRecForMulInOut
-wavRecForMulInOut.save_sensor_data_to_file(0 , 2 , filename )
+import wavGen
+
+wavGen.wavGenerator(11 , 1000)
 
 
 
